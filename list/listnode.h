314068937f746aba8d09da27c1ae3b4d8d72bb18
Cutecodes <1503310252@qq.com>
2020-04-28 05:02:34 -0400

    add listnode

---
+++ list/listnode.h
@@ -0,0 +1,17 @@
+#ifndef LISTNODE_H
+#define LISTNODE_H
+
+#define ListNodePosi(T) ListNode<T>*
+
+template <typename T>
+struct ListNode{
+	T data;
+	ListNodePosi(T) pred;
+	LIstNodePosi(T) succ;
+	ListNode(){}
+	ListNode(T e,ListNodePosi(T) p=NULL,ListNodePosi(T) s=NULL):data(e),pred(p),succ(s){}
+	ListNodePosi(T) insertAsPred(T const& e);
+	ListNodePosi(T) insertAsSucc(T const& e);
+}
+
+#endif
